1b81f469943253ca8de17cbd9e8a55bdd1150da8
shumbushofred <45537513+shumbushofred@users.noreply.github.com>
2020-04-20 17:10:43 -0500

    Updates

---
+++ precache-manifest.75479cc36a66ce9982d28d1c92771c1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea185b060a5ebc7c7d21d5e659a7aba6",
+    "url": "/clientreact/index.html"
+  },
+  {
+    "revision": "a6538ed52aebc0c771c8",
+    "url": "/clientreact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "edb1728b676f2d6f21e3",
+    "url": "/clientreact/static/css/main.96dc3664.chunk.css"
+  },
+  {
+    "revision": "a6538ed52aebc0c771c8",
+    "url": "/clientreact/static/js/2.c1345066.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/clientreact/static/js/2.c1345066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb1728b676f2d6f21e3",
+    "url": "/clientreact/static/js/main.d9ac04e5.chunk.js"
+  },
+  {
+    "revision": "19ce814e106ab0b9a1a3",
+    "url": "/clientreact/static/js/runtime-main.f836b5ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clientreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
